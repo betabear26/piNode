6a899f658fa6637031d51f6ba9e3f8ca0fd939b2
betabear26 <betabear26@gmail.com>
2020-03-11 09:47:51 +0530

    Init

--- src/app.py
+++ src/app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-return "Success",200
+def hello():
+  return "Success",200
 
 
 if __name__ == "__main__":
-    app.debug=True
-    app.run(host='0.0.0.0', 5000)
+  app.debug=True
+  app.run(host='0.0.0.0', port=5000)
